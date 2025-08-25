d337c62e62424fb921d829ce54deb08c2b1f7273
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-08-25 18:35:09 -0400

    widget to dispaly creators that are live

--- frontend/app/components/creatorsLive.tsx
+++ frontend/app/components/creatorsLive.tsx
@@ -3,20 +3,27 @@ import Image from "next/image";
 import Link from "next/link";
 import LiveCreator from "./liveCreator";
 
-function CreatorsLive() {
-  const [liveCount, setLiveCount] = useState<number>(0);
-  const count = 4;
+// List of creators currently live
+type creatorList = {
+  creators: {
+    id: number;
+    name: string;
+    logo: string;
+  }[];
+};
 
-  const creatorList = [
-    { id: 0, name: "TheOneWhoThinks", logo: "/vercel.svg" },
-    { id: 1, name: "Prabhdeep", logo: "/vercel.svg" },
-  ];
+function CreatorsLive({ creators }: creatorList) {
+  const [liveCount, setLiveCount] = useState<number>(0);
+
+  // Updates liveCount when creators prop changes
+  useEffect(() => {
+    setLiveCount(creators.length);
+  }, [creators]);
 
   return (
-    <div className="bg-indigo-200 w-56 h-56 gap-3">
+    <div className="bg-indigo-200 w-64 h-56 grid grid-cols-1 gap-1">
       <p>Number of creators live: {liveCount}</p>
-
-      {creatorList.map((creator) => {
+      {creators.map((creator) => {
         return <LiveCreator key={creator.id} creator={creator}></LiveCreator>;
       })}
     </div>
--- frontend/app/components/liveCreator.tsx
+++ frontend/app/components/liveCreator.tsx
@@ -12,7 +12,7 @@ function LiveCreator({ creator }: creatorNmae) {
   const [creatorName, setCreatorName] = useState<string>("");
 
   return (
-    <div className="bg-indigo-400 w-full h-10 top-0 z-10">
+    <div className="flex bg-indigo-400 w-full h-10 top-0 z-10 gap-2 p-2">
       <Image
         src={creator.logo}
         alt={`${creator.name} logo`}
