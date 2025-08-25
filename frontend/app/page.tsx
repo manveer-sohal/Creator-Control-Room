97da675294d7beaa9f44253c49a112c559677d0e
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-08-25 19:15:57 -0400

    incorperated events widget and button to generate the event

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,13 +5,76 @@ import CreatorsLive from "./components/creatorsLive";
 import SideBar from "./components/sideBar";
 import NavBar from "./components/navBar";
 
+import react, { useEffect, useRef, useState } from "react";
+import { io, Socket } from "socket.io-client";
+import GenerateEvent from "./components/generateEvents";
+import EventsWidget from "./components/eventsWidget";
+
+// import { EventV2 } from "../types";
+// import { EventsWidgetProps } from "../types";
+import { eventObj } from "../types";
+
+declare global {
+  // extend the global scope for dev/HMR
+  var __SOCKET__: Socket | undefined;
+}
+
 export default function Home() {
+  const socketRef = useRef<Socket | null>(null);
+  const [message, setMessage] = useState<string>("");
+  const [messages, setMessages] = useState<{ msg: string; key: string }[]>([]);
+  const [key, setKey] = useState<string>("");
+  const [event, setEvent] = useState<Event>();
+  const [displayEventList, setDisplayEventList] = useState<eventObj[]>([]);
+
   const creators = [
     { id: 0, name: "TheOneWhoThinks", logo: "/vercel.svg" },
     { id: 1, name: "Crazy1Prabh", logo: "/vercel.svg" },
     { id: 2, name: "JasonTheWeen", logo: "/vercel.svg" },
     { id: 3, name: "StableRonaldo", logo: "/vercel.svg" },
   ];
+  // FORM SUBMISSION
+  // const handleSubmit = (e: react.FormEvent) => {
+  //   e.preventDefault();
+  //   socketRef.current?.emit("chat message", { msg: message, key: key });
+  //   setMessage("");
+  //   setKey("");
+  // };
+
+  // ON HOIST USE EFFECT
+  useEffect(() => {
+    if (!globalThis.__SOCKET__) {
+      globalThis.__SOCKET__ = io("http://localhost:3001", {
+        transports: ["websocket", "polling"],
+      });
+    }
+    const s = globalThis.__SOCKET__;
+    socketRef.current = s;
+
+    // SOCKET FUNCTIONS
+
+    const onEvent = (event: eventObj) => {
+      setDisplayEventList((prev) => [...prev, event]);
+    };
+
+    // SOCKET CONNECTIONS
+
+    s.on("Event", onEvent);
+
+    //DISCONNECT SOCKETS
+    return () => {
+      s.off("Event", onHistory);
+
+      s.disconnect();
+      socketRef.current = null;
+    };
+  }, []);
+
+  // GENERATE AN EVENT REQUEST
+  const generateTheEvent = (dataFromChild: Event) => {
+    setEvent(dataFromChild);
+    socketRef.current?.emit("Event", dataFromChild);
+  };
 
   return (
     // Shell controls height + scrolling behavior
@@ -31,7 +94,9 @@ export default function Home() {
         {/* Right column (main): takes remaining space, scrolls */}
         <main className="min-w-0 overflow-y-auto p-6 gap-4">
           <div className="grid min-h-0 grid-cols-[16rem_1fr]">
+            <GenerateEvent onAction={generateTheEvent}></GenerateEvent>
             <CreatorsLive creators={creators} />
+            <EventsWidget events={displayEventList}></EventsWidget>
           </div>
         </main>
       </div>
