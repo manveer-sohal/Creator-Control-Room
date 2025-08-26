3ffceda9f896a3c6bfa6ecd364ed0885f710cd4d
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-08-26 13:11:35 -0400

    updated colour scheme

--- frontend/app/components/sideBar.tsx
+++ frontend/app/components/sideBar.tsx
@@ -13,7 +13,7 @@ function SideBar() {
   ];
 
   return (
-    <div className="bg-indigo-400 w-60 shrink-0 h-full ">
+    <div className="bg-[#26262b] shrink-0 h-full ">
       <p className="p-4 font-semibold">Sidebar</p>
       <button className="p-4 font-semibold"> click me </button>
       <button className="p-4 font-semibold"> click me </button>
