f466d2c1d77b95a07c0f54df6192bbeaa0db4aa4
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-08-27 20:13:00 -0400

    deleted unused imports

--- frontend/app/components/widgets/creatorsLive.tsx
+++ frontend/app/components/widgets/creatorsLive.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
 import LiveCreator from "../liveCreator";
 
 // List of creators currently live
