04f756a0ae4ac7d3e5f6c51eb43a0ce2b56039f2
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-08-27 20:05:51 -0400

    fixed serializeData function and moved to util file

--- backend/socket.js
+++ backend/socket.js
@@ -1,51 +1,45 @@
 import { Server } from "socket.io";
+import express from "express";
+import http from "http";
+
 import dotenv from "dotenv";
+import { serializeData } from "./socket_utils/twitchNoti_utils.js";
 dotenv.config();
 
-function serializeData(event_data) {
-  var user_name = event_data.event.user_name;
-  var broadcaster_user_name = event_data.event.broadcaster_user_name;
-  var type = event_data.subscription.type;
-  return {
-    user_name: user_name,
-    broadcaster_user_name: broadcaster_user_name,
-    type: type,
-  };
-}
+const app = express();
+const httpServer = http.createServer(app);
 
-export function initSocket(httpServer) {
-  const allowedOrigins = process.env.CORS_ORIGIN?.split(",") ?? [
-    "http://localhost:3000",
-  ];
-  const io = new Server(httpServer, {
-    cors: {
-      origin: allowedOrigins,
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
-    transports: ["websocket", "polling"],
+const allowedOrigins = process.env.CORS_ORIGIN?.split(",") ?? [
+  "http://localhost:3000",
+];
+const io = new Server(httpServer, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+  transports: ["websocket", "polling"],
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected, id:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 
-  io.on("connection", (socket) => {
-    console.log("a user connected, id:", socket.id);
-
-    socket.on("disconnect", () => {
-      console.log("user disconnected");
-    });
-
-    socket.on("Event", (arg) => {
-      var data = serializeData(arg);
-      socket.emit("Event", data);
-    });
+  socket.on("Event", (arg) => {
+    var data = serializeData(arg);
+    socket.emit("Event", data);
   });
+});
 
-  // instrument(io, {
-  //   auth: {
-  //     type: "basic",
-  //     username: adminUser,
-  //     password: adminPass, // "changeit" encrypted with bcrypt
-  //   },
-  // });
+// instrument(io, {
+//   auth: {
+//     type: "basic",
+//     username: adminUser,
+//     password: adminPass, // "changeit" encrypted with bcrypt
+//   },
+// });
 
-  return io;
-}
+export { io, httpServer, app };
--- backend/socket_utils/twitchNoti_utils.js
+++ backend/socket_utils/twitchNoti_utils.js
@@ -14,20 +14,66 @@ export function serializeDataV1(event_data) {
   };
 }
 
-export function serializeDataV2(event_data) {
-  var to_broadcaster_user_name = event_data.event.to_broadcaster_user_name;
-  var from_broadcaster_user_name = event_data.event.from_broadcaster_user_name;
+export function serializeData(event_data) {
+  var user_name = event_data.event.user_name;
+  var broadcaster_user_name = event_data.event.broadcaster_user_name;
   var type = event_data.subscription.type;
-  return {
-    from_broadcaster_user_name: from_broadcaster_user_name,
-    to_broadcaster_user_name: to_broadcaster_user_name,
-    type: type,
-  };
-}
-
-const subscription_dict = {};
-
-export function checkPayloadType(event_data) {
-  var subscription_type = event_data.payload.subscription.type;
-  console.log(subscription_type);
+  var json = {};
+  console.log("HERE-", event_data);
+  if (type == "channel.follow") {
+    json = {
+      type: "follow",
+      user_name: user_name,
+      broadcaster_user_name: broadcaster_user_name,
+    };
+  }
+  if (type == "channel.subscribe") {
+    json = {
+      type: "subscribe",
+      user_name: user_name,
+      broadcaster_user_name: broadcaster_user_name,
+      teir: event_data.event.teir,
+      is_gift: event_data.event.gift,
+    };
+  }
+  if (type == "channel.cheer") {
+    json = {
+      type: "cheer",
+      user_name: user_name,
+      broadcaster_user_name: broadcaster_user_name,
+      message: event_data.event.message, // pogchamp
+      bits: event_data.event.bits, // 1000
+    };
+  }
+  if (type == "channel.bits.use") {
+    json = {
+      type: "bits",
+      user_name: user_name,
+      broadcaster_user_name: broadcaster_user_name,
+      bits: event_data.event.bits,
+      event_type: event_data.event.event, // type: "cheer",
+      power_up: event_data.event.power_up, // power_up: null,
+      message: event_data.event.message, // This will be multiple objects
+    };
+  }
+  if (type == "channel.raid") {
+    json = {
+      type: "raid",
+      from_broadcaster_user_name: event_data.event.from_broadcaster_user_name,
+      to_broadcaster_user_name: event_data.event.to_broadcaster_user_name,
+      viewers: event_data.event.viewers,
+    };
+  }
+  if (type == "channel.subscription.gift") {
+    json = {
+      type: "gift",
+      user_name: user_name,
+      broadcaster_user_name: broadcaster_user_name,
+      teir: event_data.event.teir,
+      total: event_data.event.total, // Number of gifts
+    };
+    //cumulative_total: 284, //null if anonymous or not shared by the user
+    //is_anonymous: false,
+  }
+  return json;
 }
