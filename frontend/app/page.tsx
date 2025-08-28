9323f534fd56d917b5585684cef2ed26c0efc4d3
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-08-27 20:10:47 -0400

    refactored code: useState's combined into one for events Updated: added handaling for adding and deleting widgets, added widgets into dashboard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,23 @@
 "use client";
-
-import Image from "next/image";
 import SideBar from "./components/sideBar";
 import NavBar from "./components/navBar";
-import react, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import GenerateEvent from "./components/generateEvents";
+import BitsWidget from "./components/widgets/bitsWidget";
+import CheerWidget from "./components/widgets/cheerWidget";
 import CreatorsLive from "./components/widgets/creatorsLive";
 import EventsWidget from "./components/widgets/eventsWidget";
 import GiftedWidget from "./components/widgets/giftedWidget";
-import JoinsWidget from "./components/widgets/joins24Widget";
+// import JoinsWidget from "./components/widgets/joins24Widget";
 import NewFollowerWidget from "./components/widgets/newFollowersWidget";
-import RaidWidget from "./components/widgets/raidWidget";
+// import RaidWidget from "./components/widgets/raidWidget";
 import SubsWidget from "./components/widgets/subsWidget";
 import { UniqueViewsChart } from "./components/uniqueViewsChart";
-
-import { EventPayload } from "./../types";
-
+import TestAuth from "./components/testAuth";
 // import { EventV2 } from "../types";
 // import { EventsWidgetProps } from "../types";
-import { eventObj } from "../types";
+import { EventObj, EventPayload } from "../types";
 
 declare global {
   // extend the global scope for dev/HMR
@@ -28,13 +26,23 @@ declare global {
 
 export default function Home() {
   const socketRef = useRef<Socket | null>(null);
-  const [displayEventList, setDisplayEventList] = useState<eventObj[]>([]);
-  const [newFollowEvents, setNewFollowEvents] = useState<eventObj[]>([]);
-  const [newBitsEvents, setNewBitsEvents] = useState<eventObj[]>([]);
-  const [newRaidEvents, setNewRaidEvents] = useState<eventObj[]>([]);
-  const [newCheerEvents, setNewCheerEvents] = useState<eventObj[]>([]);
-  const [newSubEvents, setNewSubEvents] = useState<eventObj[]>([]);
-  const [newGiftedEvents, setNewGiftedEvents] = useState<eventObj[]>([]);
+
+  const [displayEventList, setDisplayEventList] = useState<EventObj[]>([]);
+  const [allEvents, setAllEvents] = useState<Record<string, EventObj[]>>({});
+  // const [newFollowEvents, setNewFollowEvents] = useState<EventObj[]>([]);
+  // const [newBitsEvents, setNewBitsEvents] = useState<EventObj[]>([]);
+  // const [newRaidEvents, setNewRaidEvents] = useState<EventObj[]>([]);
+  // const [newCheerEvents, setNewCheerEvents] = useState<EventObj[]>([]);
+  // const [newSubEvents, setNewSubEvents] = useState<EventObj[]>([]);
+  // const [newGiftedEvents, setNewGiftedEvents] = useState<EventObj[]>([]);
+  const [widgets, setWidgets] = useState<Record<string, boolean>>({
+    cheer: true,
+    raid: true,
+    bits: true,
+    subscribe: true,
+    gift: true,
+    follow: true,
+  });
 
   const creators = [
     { id: 0, name: "TheOneWhoThinks", logo: "/vercel.svg" },
@@ -46,6 +54,7 @@ export default function Home() {
 
   // ON HOIST USE EFFECT
   useEffect(() => {
+    console.log(widgets.cheer);
     if (!globalThis.__SOCKET__) {
       globalThis.__SOCKET__ = io("http://localhost:3001", {
         transports: ["websocket", "polling"],
@@ -55,28 +64,14 @@ export default function Home() {
     socketRef.current = s;
 
     // ******SOCKET FUNCTIONS*****
-
     // NEW EVENT
-    const onEvent = (event: eventObj) => {
-      // FOLLOW
-      if (event.type == "channel.follow") {
-        setNewFollowEvents((prev) => [event, ...prev]);
-      }
-      if (event.type == "channel.subscribe") {
-        setNewSubEvents((prev) => [event, ...prev]);
-      }
-      if (event.type == "channel.cheer") {
-        setNewCheerEvents((prev) => [event, ...prev]);
-      }
-      if (event.type == "channel.bits.use") {
-        setNewBitsEvents((prev) => [event, ...prev]);
-      }
-      if (event.type == "channel.raid") {
-        setNewRaidEvents((prev) => [event, ...prev]);
-      }
-      if (event.type == "channel.subscription.gift") {
-        setNewGiftedEvents((prev) => [event, ...prev]);
-      }
+    const onEvent = (event: EventObj) => {
+      console.log(event);
+
+      setAllEvents((prev) => ({
+        ...prev,
+        [event.type]: [event, ...(prev[event.type] || [])],
+      }));
       setDisplayEventList((prev) => [event, ...prev]);
     };
 
@@ -89,6 +84,7 @@ export default function Home() {
       s.off("Event", onEvent);
 
       s.disconnect();
+      console.log(socketRef.current);
       socketRef.current = null;
     };
   }, []);
@@ -96,6 +92,20 @@ export default function Home() {
   // GENERATE AN EVENT REQUEST
   const generateTheEvent = (dataFromChild: EventPayload) => {
     socketRef.current?.emit("Event", dataFromChild);
+  };
+
+  const widgetStateChange = ({
+    widgetName,
+    widgetState,
+  }: {
+    widgetName: string;
+    widgetState: boolean;
+  }) => {
+    console.log(widgetName, widgetState);
+    setWidgets((prev) => ({
+      ...prev,
+      [widgetName]: widgetState,
+    }));
   };
 
   return (
@@ -106,12 +116,13 @@ export default function Home() {
         <NavBar />
       </header>
 
-      {/* Content row: 2 columns */}
+      {/* Content row: 3 columns */}
       <div className="grid min-h-0 grid-cols-[16rem_1fr_18rem]">
         {/* Left column (sidebar): fixed width */}
         <aside className="min-h-0 overflow-y-auto">
-          <SideBar />
+          <SideBar onAction={widgetStateChange}></SideBar>
           <GenerateEvent onAction={generateTheEvent}></GenerateEvent>
+          <TestAuth></TestAuth>
         </aside>
 
         {/* Right column (main): takes remaining space, scrolls */}
@@ -119,12 +130,34 @@ export default function Home() {
           <div className="grid-rows-[auto_auto_auto]">
             <div className="grid min-h-0 grid-cols-[auto_auto_auto] gap-0.5">
               <CreatorsLive creators={creators} />
-              <GiftedWidget events={newGiftedEvents}> </GiftedWidget>
+              <GiftedWidget
+                onAction={widgetStateChange}
+                events={allEvents["gift"] || []}
+                show={widgets.gift}
+              />
+              <CheerWidget
+                onAction={widgetStateChange}
+                events={allEvents["cheer"] || []}
+                show={widgets.cheer}
+              />
             </div>
             <div className="grid min-h-0 grid-cols-[auto_auto_auto] gap-0.5">
-              <SubsWidget events={newSubEvents}></SubsWidget>
-              <CreatorsLive creators={creators} />
-              <NewFollowerWidget events={newFollowEvents}></NewFollowerWidget>
+              <SubsWidget
+                onAction={widgetStateChange}
+                events={allEvents["subscribe"] || []}
+                show={widgets.subscribe}
+              ></SubsWidget>
+              <BitsWidget
+                onAction={widgetStateChange}
+                events={allEvents["bits"] || []}
+                show={widgets.bits}
+              ></BitsWidget>
+              <NewFollowerWidget
+                onAction={widgetStateChange}
+                events={allEvents["follow"] || []}
+                show={widgets.follow}
+              ></NewFollowerWidget>
+              {/* <RaidWidget events={newRaidEvents}></RaidWidget> */}
             </div>
             <UniqueViewsChart></UniqueViewsChart>
           </div>
