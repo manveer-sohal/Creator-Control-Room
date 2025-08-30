11818079c6a410b16ab2ac13d58f64623135b075
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-08-30 14:50:16 -0400

    added 4 more subscribe event functions and function to call all subscribe events

--- backend/server_utils/authCallback_util.js
+++ backend/server_utils/authCallback_util.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const CLIENT_ID_WEBHOOK = process.env.CLIENT_ID_WEBHOOK;
+const CLIENT_SECRET_WEBHOOK = process.env.CLIENT_ID_WEBHOOK;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.SECRET;
 const REDIRECT_URI_AUTH = process.env.REDIRECT_URI_AUTH;
@@ -85,10 +85,159 @@ export async function SubscribeToFollowEvent(
         transport: {
           method: "webhook",
           callback: REDIRECT_URI_WEBHOOK,
-          secret: CLIENT_ID_WEBHOOK,
+          secret: CLIENT_SECRET_WEBHOOK,
         },
       }),
     }
   );
   const data = await response_to_subscribe.json();
 }
+
+export async function SubscribeToGiftEvent(app_accses_token, broadcaster_id) {
+  const response_to_gift = await fetch(
+    "https://api.twitch.tv/helix/eventsub/subscriptions",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${app_accses_token}`,
+        "Client-Id": CLIENT_ID,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "channel.subscription.gift",
+        version: "1",
+        condition: {
+          broadcaster_user_id: broadcaster_id,
+        },
+        transport: {
+          method: "webhook",
+          callback: "https://example.com/webhooks/callback",
+          secret: CLIENT_SECRET_WEBHOOK,
+        },
+      }),
+    }
+  );
+  const data = await response_to_gift.json();
+}
+
+export async function SubscribeToRaidEvent(app_accses_token, broadcaster_id) {
+  const response_to_raid = await fetch(
+    "https://api.twitch.tv/helix/eventsub/subscriptions",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${app_accses_token}`,
+        "Client-Id": CLIENT_ID,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "channel.raid",
+        version: "1",
+        condition: {
+          to_broadcaster_user_id: broadcaster_id,
+        },
+        transport: {
+          method: "webhook",
+          callback: "https://example.com/webhooks/callback",
+          secret: CLIENT_SECRET_WEBHOOK,
+        },
+      }),
+    }
+  );
+  const data = await response_to_raid.json();
+}
+
+export async function SubscribeToCheerEvent(app_accses_token, broadcaster_id) {
+  const response_to_cheer = await fetch(
+    "https://api.twitch.tv/helix/eventsub/subscriptions",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${app_accses_token}`,
+        "Client-Id": CLIENT_ID,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "channel.cheer",
+        version: "1",
+        condition: {
+          broadcaster_user_id: broadcaster_id,
+        },
+        transport: {
+          method: "webhook",
+          callback: "https://example.com/webhooks/callback",
+          secret: CLIENT_SECRET_WEBHOOK,
+        },
+      }),
+    }
+  );
+  const data = await response_to_cheer.json();
+}
+
+export async function SubscribeToBitsEvent(app_accses_token, broadcaster_id) {
+  const response_to_bits = await fetch(
+    "https://api.twitch.tv/helix/eventsub/subscriptions",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${app_accses_token}`,
+        "Client-Id": CLIENT_ID,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "channel.bits.use",
+        version: "1",
+        condition: {
+          broadcaster_user_id: broadcaster_id,
+        },
+        transport: {
+          method: "webhook",
+          callback: "https://example.com/webhooks/callback",
+          secret: CLIENT_SECRET_WEBHOOK,
+        },
+      }),
+    }
+  );
+  const data = await response_to_bits.json();
+}
+
+export async function subscribeToAllEvent(
+  app_accses_token,
+  broadcaster_id,
+  moderator_user_id
+) {
+  const data_subscribe_cheer = SubscribeToCheerEvent(
+    app_accses_token,
+    broadcaster_id
+  );
+  console.log(await data_subscribe_cheer);
+
+  const data_subscribe_follow = SubscribeToFollowEvent(
+    app_accses_token,
+    broadcaster_id,
+    moderator_user_id
+  );
+  console.log(await data_subscribe_follow);
+
+  const data_subscribe_gift = SubscribeToGiftEvent(
+    app_accses_token,
+    broadcaster_id
+  );
+  console.log(await data_subscribe_gift);
+
+  const data_subscribe_raid = SubscribeToRaidEvent(
+    app_accses_token,
+    broadcaster_id
+  );
+  console.log(await data_subscribe_raid);
+
+  const data_subscribe_bits = SubscribeToBitsEvent(
+    app_accses_token,
+    broadcaster_id
+  );
+  console.log(await data_subscribe_bits);
+
+  return 200;
+}
+
+//channel.chat.message
