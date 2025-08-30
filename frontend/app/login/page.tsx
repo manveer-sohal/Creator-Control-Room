be31918b786c3073ce9def9e65cb40b2e11374e2
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-08-30 14:51:45 -0400

    added login page

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import TestAuth from "../components/testAuth";
+export default function Login() {
+  return (
+    <div className="bg-red-600 p-1.5 align-middle">
+      <TestAuth />
+    </div>
+  );
+}
