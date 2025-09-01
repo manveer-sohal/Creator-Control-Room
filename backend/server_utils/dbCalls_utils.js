6869779e9971d5199e46c6f8fe154c9a705ab727
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-09-01 15:28:08 -0400

    created function to push events to db and select values from db

--- backend/server_utils/dbCalls_utils.js
+++ backend/server_utils/dbCalls_utils.js
@@ -1,16 +1,34 @@
 import pool from "../libs/postgreSQL.js";
 
 // Get Company_id from creator table
-export async function getCompanyId(creator_name) {
+export async function getIDsForEvent(creator_name) {
   try {
     const result = await pool.query(
-      "SELECT company_id FROM events WHERE name = $1",
+      "SELECT company_id, id FROM creator WHERE name = $1",
       [creator_name]
     );
-    console.log(result.row[0]);
-    return result.row[0];
+
+    return result.rows[0];
   } catch (err) {
     console.error(err);
     return [];
+  }
+}
+
+export async function pushEvent(
+  subscription_type,
+  creator_id,
+  company_id,
+  payload
+) {
+  try {
+    const result = await pool.query(
+      "INSERT INTO events (id, subscription_type, creator_id, company_id, payload, created_at) VALUES (gen_random_uuid(), $1, $2, $3, $4, NOW())",
+      [subscription_type, creator_id, company_id, payload]
+    );
+    res.json(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error inserting user");
   }
 }
