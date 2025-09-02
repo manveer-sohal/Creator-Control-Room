50d0944658bc9efedb0a6314b5c6388625dce8a7
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-09-02 14:36:45 -0400

    added s3 and twitch static logo hostnames for dispalying images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "l0g0.s3.us-east-2.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static-cdn.jtvnw.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
